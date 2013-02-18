a6660eeda9b86a913d10fcd2a079ab18e3648158
Soukaina <soukaina@soukaina-ThinkPad-Edge.(none)>
2013-02-18 16:35:55 +0100

    essai crossvalidation

--- code/digits_svm.py
+++ code/digits_svm.py
@@ -9,7 +9,7 @@ from sklearn.svm import SVC
 
 from sklearn.multiclass import OneVsRestClassifier
 
-svm = SVC()
+svm = SVC(C=.1)
 
 ovrest_svm = OneVsRestClassifier(svm)
 
@@ -22,5 +22,14 @@ test_target= target[1000:]
 ovrest_svm.fit(train_data, train_target)
 
 prediction= ovrest_svm.predict(test_data)
+
+from sklearn.cross_validation import cross_val_score
+
+score = cross_val_score(ovrest_svm, data, target, cv=5)
+
+clfs = [OneVsRestClassifier(SVC(C=alpha)) for alpha in np.logspace(-5, 1, 10)]
+
+scores = [cross_val_score(clf, data, target, cv=5) for clf in clfs]
+
 
 
