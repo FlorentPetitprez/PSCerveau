fd9ec56cab9e097b2a6e3b4d536562ac8362a231
Michael <michael.eickenberg@gmail.com>
2013-03-28 17:08:08 +0100

    prediction premiere couche

---
+++ code/multi_select_k_best.py
@@ -0,0 +1,37 @@
+# We need to reduce the number of features to very few relevant voxels
+# f_classif works with one target, but not several
+
+from sklearn.base import BaseEstimator
+from sklearn.feature_selection import f_classif
+import numpy as np
+
+class MultiSelectKBest(BaseEstimator):
+
+    def __init__(self, classif_func=f_classif, k=10,
+                 pooling_function=np.mean):
+        self.classif_func = classif_func
+        self.k = k
+        self.pooling_function = pooling_function
+
+    def fit(self, X, y):
+        scores = []
+        if y.ndim == 1:
+            y = y[:, np.newaxis]
+
+        for yy in y.T:
+            score, _ = self.classif_func(X, yy)
+            scores.append(score)
+
+        self.scores = self.pooling_function(np.array(scores), axis=0)
+
+        return self
+
+    def get_support(self):
+        if self.scores is not None:
+            res = np.zeros(self.scores.shape, dtype=np.bool)
+            res[self.scores.argsort()[::-1][:self.k]] = True
+            return res
+
+    def transform(self, X):
+        supp = self.get_support()
+        return X[:, supp]
